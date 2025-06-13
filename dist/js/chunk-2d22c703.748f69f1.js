e6f984fb5c157957d75a1941fefb13e71b151c6d
wgx1187584267 <49746438+wgx1187584267@users.noreply.github.com>
2025-06-13 13:49:53 +0800

    Delete dist/js directory

--- dist/js/chunk-2d22c703.748f69f1.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22c703"],{f2de:function(n,w){}}]);
--- dist/js/chunk-6c308ef1.3ee02e3b.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-6c308ef1"],{d3bc4:function(n,c,w){}}]);
